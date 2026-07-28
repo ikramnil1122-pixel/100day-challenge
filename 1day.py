bd3d961f9b106cfb5246f1305417b776a4404b21
Ikram Nil <ikramnil1122@gmail.com>
2026-07-28 19:57:46 +0100

    Day 1: Band Name Generator

---
+++ 1day.py
@@ -0,0 +1,10 @@
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grow up in? ")
+pet = input("What is the name of your first pet? ")
+print("Your band name is " + city + " " + pet)
+# 2 exercice
+name = input("What is your name? ")
+colore = input("What is your favorite color? ")
+food = input("What is your favorite food? ")
+print("Hello " + name + "!\n Your favorite color is " +
+      colore + " and you like " + food)
