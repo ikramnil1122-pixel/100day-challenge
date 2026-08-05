8345eee8e0e7a185dde1013266ea03ad21dd1fe8
Ikram Nil <ikramnil1122@gmail.com>
2026-08-05 20:54:18 +0100

    Day 11 - The Blackjack Capstone Project

--- day11.py
+++ day11.py
@@ -1,15 +1,63 @@
-def fuzz_buzz():
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
+# The Blackjack Capstone Project
+# Concepts Practised
+# Refactoring and calling procedures
 
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
+
+import random
+cart = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
+
+def draw():
+    e = random.choice(cart)
+    your_cards.append(e)
+
+
+def draw_computer():
+    while sum(computer_cards) < 17:
+        f = random.choice(cart)
+        computer_cards.append(f)
+
+
+def result():
+    if sum(your_cards) > 21 and 11 in your_cards:
+        your_cards.remove(11)
+        your_cards.append(1)
+    resulte_you = sum(your_cards)
+    if sum(computer_cards) > 21 and 11 in computer_cards:
+        computer_cards.remove(11)
+        computer_cards.append(1)
+    resulte_computer = sum(computer_cards)
+    return resulte_you, resulte_computer
+
+
+def compare(resulte_you, resulte_computer):
+    if resulte_you > 21:
+        print("you went over. you lose")
+    elif resulte_computer > 21:
+        print("computer went over. you win")
+    elif resulte_you == 21 and resulte_computer == 21:
+        print("draw")
+    elif resulte_you > resulte_computer:
+        print("you win")
     else:
-        print(number)
-    return
+        print("you lose")
 
 
-number = int(input("Enter a number: "))
-fuzz_buzz()
+[a, b] = random.choice(cart, 2)
+[c, d] = random.choice(cart, 2)
+your_cards = [a, b]
+computer_cards = [c, d]
+
+print(f'your cards is:[{a} : {b}]')
+print(f'the computer\'s first cards is:[{c} : {'*'}]')
+game_over = False
+while not game_over:
+    if result_computer() == 21:
+        print("computer has a blackjack. you lose")
+
+    input = str(input(
+        'type n if you want to draw another card or type s if you want to stop drawing cards'))
+    if input == 'n':
+        draw()
+    else:
+        compare()
