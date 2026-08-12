91202fd8050873575f79894097bb75895b66fc2a
Ikram Nil <ikramnil1122@gmail.com>
2026-08-12 20:26:39 +0100

    day15 coffee machine

---
+++ day15/day15.py
@@ -0,0 +1,24 @@
+# Local Development Environment Setup & the Coffee Machine
+# Concepts Practised
+# Installing Python Locally
+def espresso():
+
+
+def cappoccino():
+
+
+def latte():
+
+
+choice = input(what do you like? type 'esprisso', 'latte', or 'cappoccino')
+print = ("please insert coins")
+quartes = input(how many quartes)
+dimmes = input(how many dimmes)
+nickles = input(how many nickles)
+pannies = input(how many pannies)
+if choice == espresso:
+    espresso
+elif choice == cappoccino:
+    cappoccino
+else:
+    latte
